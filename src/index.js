6b2eb9a981ce2be3aafab1e87ab89e5012deb215
Peter Gyory <pgyory@sapient.com>
2016-10-24 18:47:47 -0400

    set eyeball colors and disperse

--- src/index.js
+++ src/index.js
@@ -5,6 +5,15 @@ import * as THREE from 'three';
 
 import newSphere from './sphere';
 
+let SPHERE_STATE = 'EYE'; // EYE
+let switchTime = 0;
+let moveInterval = 2000;
+
+const swapEyeState = () => {
+  SPHERE_STATE = SPHERE_STATE === 'EYE' ? 'DISPERSE' : 'EYE';
+  switchTime = Date.now();
+}
+
 const genSphereRow = ( theta, numZ ) =>
   R.unfold(
     n => n > numZ ? false : [ newSphere( theta, n / numZ ), n + 1 ],
@@ -16,6 +25,24 @@ const genSpheres = ( numY, numZ ) =>
     n => n > numY ? false : [ genSphereRow( n / numY, numZ ), n + 1 ],
     0
   );
+
+const helpVec = new THREE.Vector3();
+const updateSphere = sphere => {
+  if ( Date.now() - switchTime <= moveInterval ) {
+    const { x, y, z } = sphere.position;
+    const targetPos = SPHERE_STATE === 'EYE' ? sphere.eyePos : sphere.randPos;
+
+//    sphere.material.color = new THREE.color
+
+    sphere.position.set(
+      ( ( targetPos.x - x ) * ( Date.now() - switchTime ) / moveInterval ) + x,
+      ( ( targetPos.y - y ) * ( Date.now() - switchTime ) / moveInterval ) + y,
+      ( ( targetPos.z - z ) * ( Date.now() - switchTime ) / moveInterval ) + z,
+    );
+  }
+};
+
+
 
 window.onload = () => {
   // Camera Setup
@@ -49,19 +76,22 @@ window.onload = () => {
   scene.add( lights[ 1 ] );
   scene.add( lights[ 2 ] );
 
-  R.flatten(genSpheres( 20, 10 ))
+  const eyeSpheres = R.flatten(genSpheres( 25, 25 ));
+  eyeSpheres
     .forEach( sphere => scene.add( sphere ) );
+
+  swapEyeState();
 
   var render = function () {
 
   	requestAnimationFrame( render );
 
-  	// var time = Date.now() * 0.001;
-    //
-  	// scene.rotation.x += 0.005;
-  	// scene.rotation.y += 0.005;
-    scene.rotation.x = 0.4;
-  	scene.rotation.y = 0.4;
+    eyeSpheres.forEach( updateSphere );
+
+    if ( SPHERE_STATE === 'DISPERSE' ) {
+      scene.rotation.x += 0.004;
+    	scene.rotation.y += 0.004;
+    }
 
   	effect.render( scene );
 
@@ -77,4 +107,6 @@ window.onload = () => {
   }, false );
 
   render();
+
+  document.querySelector('canvas').addEventListener( 'click', swapEyeState);
 }
--- src/sphere.js
+++ src/sphere.js
@@ -12,17 +12,27 @@ const genSphereCords = (theta1, theta2) =>
 
 export default function ( theta, zeta ) {
   const [ x, y, z ] = genSphereCords( theta, zeta );
-  const r = z > 0 ? z / EYE_SIZE : 0;
-  const g = z > 0 ? z / EYE_SIZE : 0;
-  const b = z > 0 ? 1 : ( EYE_SIZE + z ) / EYE_SIZE;
-  const color = z < EYE_SIZE ? new THREE.Color( r, g, b ) : 0xff0000;
-  const rad = z < EYE_SIZE ? 1 : 2.5;
+  let r = z > EYE_SIZE - 6 && z < EYE_SIZE - 1 ? 0 : z / EYE_SIZE + 0.6;
+  let g = z > EYE_SIZE - 6 && z < EYE_SIZE - 1 ? 0 : z / EYE_SIZE + 0.6;
+  let b = z > EYE_SIZE - 6 && z < EYE_SIZE - 1 ? 0 : z / EYE_SIZE + 0.6;
 
-  const mat = new THREE.MeshLambertMaterial( { color, opacity: 0.7, transparent: true } );
+  const color = z < EYE_SIZE - 1 ? new THREE.Color( r, g, b ) : 0xff0000;
+
+  const rad = z < EYE_SIZE ? 0.5 : 1;
+
+  const mat = new THREE.MeshLambertMaterial( { color, transparent: true } );
   const geo = new THREE.SphereGeometry( rad, 10, 10 );
   const sphere = new THREE.Mesh( geo, mat );
   sphere.position.set( x, y, z );
+  sphere.eyePos = { x, y, z };
+  sphere.randPos = {
+    x: Math.random() * 60 - 30,
+    y: Math.random() * 60 - 30,
+    z: Math.random() * 60 - 30,
+  };
 
+  sphere.color = { r, g, b };
+  sphere.currentColor = { r, g, b };
 
   return sphere;
 };
